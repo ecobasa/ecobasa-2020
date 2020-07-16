b0733821bb0279e32e84a82efb0b0cd943d47109
Henrik Kröger <hedwig@riseup.net>
2020-07-16 15:00:22 +0200

    Add notifications to logout as well and use post

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ app_name = "users"
 
 urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
-    path("logout", views.LogoutView.as_view(), name="logout"),
+    path("logout", views.logout, name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,17 @@
-from django.contrib.auth.views import (
-    LoginView as DjangoLoginView,
-    LogoutView as DjangoLogoutView,
-)
+from django.contrib.auth.views import LoginView as DjangoLoginView
+from django.utils.translation import gettext_lazy as _
+from django.shortcuts import redirect, reverse
+from django.contrib import messages
+from django.contrib.auth import logout as auth_logout
 
 
 class LoginView(DjangoLoginView):
     template_name = "users/login.html"
 
 
-class LogoutView(DjangoLogoutView):
-    pass
+def logout(request):
+    """Logout user on POST requests (more secure)"""
+    if request.method == "POST":
+        auth_logout(request)
+        messages.success(request, _("You have been logged out."))
+    return redirect(reverse("homepage:homepage"))
