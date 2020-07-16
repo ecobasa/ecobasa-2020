15be9e4c7a4010323b9582638e6649dad41786e0
Henrik Kröger <hedwig@riseup.net>
2020-07-17 00:14:23 +0200

    orange focus ring for all inputs

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,24 @@ module.exports = {
     // https://github.com/tailwindcss/custom-forms/blob/master/src/defaultOptions.js
     customForms: theme => ({
       default: {
+        input: {
+          '&:focus': {
+            boxShadow: "0 0 0 3px rgba(253, 186, 140, 0.45)",
+            borderColor: theme('color.orange-500'),
+          },
+        },
         checkbox: {
+          '&:focus': {
+            boxShadow: "0 0 0 3px rgba(253, 186, 140, 0.45)",
+            borderColor: theme('color.orange-500'),
+          },
           color: theme('colors.orange.500'),
         },
         radio: {
+          '&:focus': {
+            boxShadow: "0 0 0 3px rgba(253, 186, 140, 0.45)",
+            borderColor: theme('color.orange-500'),
+          },
           color: theme('colors.orange.500'),
         },
       },
@@ -24,9 +38,6 @@ module.exports = {
       color: {
         "orange-500": "#da6136"
       },
-      boxShadow: {
-        outline: '0 0 0 3px #da6136',
-      }
     },
   },
   variants: {},
