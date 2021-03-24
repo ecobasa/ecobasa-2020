eded2b96c6665888a5b547d9f82ef25cece69f28
ArneBo <arne@ecobasa.org>
2021-03-24 20:20:55 +0100

    content page for support and donations

--- homepage/views.py
+++ homepage/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, "homepage/homepage.html")
+
+def support(request):
+    return render(request, "homepage/support.html")
